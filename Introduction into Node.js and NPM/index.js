ed2838e468e8af512b8a5debcfa211853600114c
SaadHussein <sa3dhusseein@gmail.com>
2024-02-11 14:21:40 +0200

    Reviewing about routing and url

--- Introduction into Node.js and NPM/index.js
+++ Introduction into Node.js and NPM/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
-
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const text = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(text);
@@ -24,9 +25,36 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 });
 console.log('Will Read File ?');
 
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 
 const server = http.createServer((req, res) => {
-    res.end('Hello From Server.');
+    const { query, pathname } = url.parse(req.url, true);
+
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-Type': "text/html" });
+        const cardsHtml = dataObj.map((el) => replaceTemplate(templateCard, el)).join('');
+        const output = templateOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+        res.end(output);
+    } else if (pathname === '/product') {
+        const product = dataObj[query.id];
+        const output = replaceTemplate(templateProduct, product);
+        res.end(output);
+    } else if (pathname === '/api') {
+        res.writeHead(200, { 'Content-Type': "application/json" });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-Type': "text/html",
+            "My-Own-Header": "Hello World"
+        });
+        res.end('<h1>Page Not Found</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
