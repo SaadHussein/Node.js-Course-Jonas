1758d1aa60774ccb4bf5d135ab6c2c12c8b16321
SaadHussein <sa3dhusseein@gmail.com>
2024-03-05 03:02:07 +0200

    Finish Error Handling Section, Awesome Section

--- Natours API/controllers/errorController.js
+++ Natours API/controllers/errorController.js
@@ -1,9 +1,70 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = (err) => {
+    // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; //To Match Any String Between Qoutes.
+    // console.log(value);
+    const value = Object.values(err.keyValue)[0];
+    const message = `Duplicate Field Value: ${value}, Please Use Another Value`;
+    return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map((err) => err.message);
+
+    const message = `Invalid Input Data, ${errors.join(', ')}`;
+    return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack
+    });
+};
+const sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        res.status(500).json({
+            status: "error",
+            message: "Something Went Wrong..!"
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+        error.name = err.name;
+        error.code = err.code;
+
+        if (error.name === 'CastError') {
+            error = handleCastErrorDB(error);
+        }
+
+        if (error.code === 11000) {
+            error = handleDuplicateFieldDB(error);
+        }
+
+        if (error.name === 'ValidationError') {
+            error = handleValidationErrorDB(error);
+        }
+
+        sendErrorProd(error, res);
+    }
 };
--- Natours API/server.js
+++ Natours API/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
+
+process.on('uncaughtException', (err) => {
+    console.log('Uncaught Exception, Shutting Down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 const app = require('./app');
 
 mongoose.connect(process.env.DATABASE).then(() => {
@@ -8,6 +15,14 @@ mongoose.connect(process.env.DATABASE).then(() => {
 
 const Port = process.env.PORT || 3000;
 
-app.listen(Port, () => {
+const server = app.listen(Port, () => {
     console.log('App Running Now.');
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log('Unhandled Rejection, Shutting Down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
