f8fb9ade24d221b253720faa9c037fa6c689368c
SaadHussein <sa3dhusseein@gmail.com>
2024-03-06 01:54:36 +0200

    works on authentication and authorization, finish login, signup, forgot password and some middlewares

---
+++ Natours API/controllers/authController.js
@@ -0,0 +1,111 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+};
+
+const signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+        role: req.body.role
+
+    });
+
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+        status: "success",
+        token,
+        data: {
+            user: newUser
+        }
+    });
+});
+
+const login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new AppError('Please Provide an Email and Password.', 400));
+    }
+
+    const user = await User.findOne({ email: email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect Email or Password', 401));
+    }
+
+    const token = signToken(user._id);
+
+    res.status(200).json({
+        status: "success",
+        token
+    });
+});
+
+const protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization || req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        return next(new AppError('Your are Not Logged In, Log In First.', 401));
+    }
+
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    const currentUser = await User.findById(decoded.id);
+
+    if (!currentUser) {
+        return next(new AppError('The User Belonging To This Token No Longer Exist', 401));
+    }
+
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(new AppError('The User Recently Change Password, Please Login Again', 401));
+    };
+
+    req.user = currentUser;
+    next();
+});
+
+const restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new AppError('You Do Not Have Permission To Do This Action', 403));
+        }
+
+        next();
+    };
+};
+
+const forgotPassword = catchAsync(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next('There is No User with This Email Address.', 404);
+    }
+
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+});
+
+const resetPassword = (req, res, next) => { };
+
+module.exports = {
+    signup,
+    login,
+    protect,
+    restrictTo,
+    forgotPassword,
+    resetPassword
+};
