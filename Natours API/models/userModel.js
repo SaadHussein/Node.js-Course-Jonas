5f5f8016245c30e8d611ca898486a130eeccafb5
SaadHussein <sa3dhusseein@gmail.com>
2024-03-07 22:39:23 +0200

    Finish Delete Me Route

--- Natours API/models/userModel.js
+++ Natours API/models/userModel.js
@@ -40,7 +40,12 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 userSchema.pre('save', async function (next) {
@@ -60,6 +65,12 @@ userSchema.pre('save', function (next) {
     }
 
     this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre(/^find/, (next) => {
+    this.find({ active: { $ne: false } });
+
     next();
 });
 
--- Natours API/routes/userRoutes.js
+++ Natours API/routes/userRoutes.js
@@ -5,7 +5,8 @@ const {
     getUser,
     updateUser,
     deleteUser,
-    updateMe
+    updateMe,
+    deleteMe
 } = require('../controllers/userController');
 const { signup, login, forgotPassword, resetPassword, updatePassword, protect } = require('../controllers/authController');
 
@@ -19,6 +20,7 @@ router.patch('/resetPassword/:token', resetPassword);
 
 router.patch('/updateMyPassword', protect, updatePassword);
 router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
