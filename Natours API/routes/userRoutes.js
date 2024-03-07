16e9c87217eda17fc364c516c3f4383e4581f3ac
SaadHussein <sa3dhusseein@gmail.com>
2024-03-07 17:38:46 +0200

    Finish Update Password

--- Natours API/routes/userRoutes.js
+++ Natours API/routes/userRoutes.js
@@ -6,7 +6,7 @@ const {
     updateUser,
     deleteUser
 } = require('../controllers/userController');
-const { signup, login, forgotPassword, resetPassword } = require('../controllers/authController');
+const { signup, login, forgotPassword, resetPassword, updatePassword, protect } = require('../controllers/authController');
 
 
 const router = express.Router();
@@ -15,6 +15,8 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
+
+router.patch('/updateMyPassword', protect, updatePassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
