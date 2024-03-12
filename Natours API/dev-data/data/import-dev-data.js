6adc68ff49bbaa509c2087d709a0fb0ca9286c4d
SaadHussein <sa3dhusseein@gmail.com>
2024-03-12 16:12:10 +0200

    make indexing for price, slug and average Rating and also claculate average rating for each tour

--- Natours API/dev-data/data/import-dev-data.js
+++ Natours API/dev-data/data/import-dev-data.js
@@ -2,17 +2,23 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 mongoose.connect(process.env.DATABASE).then(() => {
     console.log('Connected Successfully.');
 });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 const importData = async () => {
     try {
         await Tour.create(tours);
-        console.log('Tours added Successfully.');
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
+        console.log('Tours, Users and Reviews added Successfully.');
         process.exit();
     } catch (err) {
         console.log(err);
@@ -22,7 +28,9 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
-        console.log('Tours deleted Successfully.');
+        await User.deleteMany();
+        await Review.deleteMany();
+        console.log('Tours, Users and Reviews deleted Successfully.');
         process.exit();
     } catch (err) {
         console.log(err);
