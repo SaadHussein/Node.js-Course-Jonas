1c36f912f734c1deb6dfddbd970bc427cc666703
SaadHussein <sa3dhusseein@gmail.com>
2024-03-15 12:50:57 +0200

    Start working with PUG

--- Natours API/app.js
+++ Natours API/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -10,8 +11,14 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(helmet());
 
@@ -35,14 +42,12 @@ app.use(hpp({
     whitelist: ['duration', 'price', 'maxGroupSize', 'ratingsQuantity', 'ratingsAverage', 'difficulty']
 }));
 
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
@@ -59,9 +64,6 @@ app.all('*', (req, res, next) => {
 
     next(new AppError(`Can Not Find ${req.originalUrl} on This Server.`, 404));
 });
-
-
-
 
 app.use(globalErrorHandler);
 
