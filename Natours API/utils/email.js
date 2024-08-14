4719750455aff2c6afd2a117089aea51a2ddb906
SaadHussein <sa3dhusseein@gmail.com>
2024-08-14 15:14:02 +0300

    working on update email service

--- Natours API/utils/email.js
+++ Natours API/utils/email.js
@@ -1,23 +1,71 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { htmlToText } = require("html-to-text");
 
-const sendEmail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(" ")[0];
+        this.url = url;
+        this.from = process.env.EMAIL_FROM;
+    }
+
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            return 1;
         }
-    });
 
-    const mailOptions = {
-        from: "Saad Hussein - Natours API",
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-    };
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+    }
 
-    await transporter.sendMail(mailOptions);
+    async send(template, subject) {
+        const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject: subject,
+            html,
+            text: htmlToText(html)
+        };
+
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome To Natours Family.');
+    }
 };
 
-module.exports = sendEmail;
+// const sendEmail = async (options) => {
+// const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         pass: process.env.EMAIL_PASSWORD
+//     }
+// });
+
+// const mailOptions = {
+//     from: "Saad Hussein - Natours API",
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message
+// };
+
+// await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
