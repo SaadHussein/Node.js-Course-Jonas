7eacbf03f430586e3c1a59451c19a50d8ab36f90
SaadHussein <sa3dhusseein@gmail.com>
2024-08-14 14:07:26 +0300

    add feature: add upload images with multer

--- Natours API/routes/tourRoutes.js
+++ Natours API/routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-    getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan, getToursWithin, getDistances
+    getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan, getToursWithin, getDistances, resizeTourImages, uploadTourImages
 } = require('../controllers/tourController');
 const { protect, restrictTo } = require('../controllers/authController');
 const reviewRouter = require('./reviewRoutes');
@@ -15,6 +15,6 @@ router.route('/monthly-plan/:year').get(protect, restrictTo("admin", "lead-guide
 router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(getToursWithin);
 router.route('/distances/:latlng/unit/:unit').get(getDistances);
 router.route('/').get(getAllTours).post(protect, restrictTo("admin", "lead-guide"), createTour);
-router.route('/:id').get(getTour).patch(protect, restrictTo("admin", "lead-guide"), updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+router.route('/:id').get(getTour).patch(protect, restrictTo("admin", "lead-guide"), uploadTourImages, resizeTourImages, updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
